27739214405aeb7ad643b8767f817f919c54c014
Koushik Dey <koushik.dey@live.com>
2023-03-01 00:21:45 +0200

    configured container

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,7 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-
-const mount = () => {
-
+const mount = (el) => {
+  ReactDOM.render(<App />, el);
 };
+
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.querySelector('#_marketing-dev-root');
+
+  if (devRoot) {
+    mount(devRoot);
+  }
+}
 
 export { mount };
