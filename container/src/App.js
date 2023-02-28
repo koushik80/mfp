6a6ff35772c2139577e95f5ac8a8120896dad7e9
Koushik Dey <koushik.dey@live.com>
2023-03-01 01:35:22 +0200

    integrated container and marketing app

--- container/src/App.js
+++ container/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import MarketingApp from './components/MarketingApp'
 
 const App = () => {
   return (
     <div>
-      <h1>Hello Container!</h1>
+      <h1>This is Container!</h1>
+      <hr />
+      <MarketingApp />
     </div>
   );
 }
